737226c289cd9c233e23d30d5c7a0b507f434251
superlou <lousimons@gmail.com>
2016-03-21 21:00:25 -0400

    Switch forces to have a heading

--- server/ship.rb
+++ server/ship.rb
@@ -21,7 +21,6 @@ class Ship < Model
     @body = CP::Body.new(@cm.mass, @cm.moi)
 
     @isAccelerating = false
-    @isDecelerating = false
 
     config = load_config('ship1')
     @terminals = config[:terminals].map do |terminal|
@@ -36,45 +35,28 @@ class Ship < Model
   end
 
   def update(dt)
-    if @body.f.y > 0
-      @isAccelerating = true
-      @isDecelerating = false
-    elsif @body.f.y < 0
-      @isAccelerating = false
-      @isDecelerating = true
-    else
-      @isAccelerating = false
-      @isDecelerating = false
+    @body.reset_forces()
+
+    if @isAccelerating
+      angle = @body.a
+      @body.apply_force(vec2(100, 0).rotate(CP::Vec2.for_angle(angle)), vec2(0, 0))
     end
   end
 
-  def terminal_briefs
-    @terminals.map{|terminal| terminal.brief}
+  def execute(details)
+    case details
+    when 'accelerate'
+      @isAccelerating = true
+    when 'em_stop'
+      @isAccelerating = false
+    end
   end
 
   def eval_data(template)
     eval(template)
   end
 
-  def execute(details)
-    case details
-    when 'accelerate'
-      @body.reset_forces()
-      @body.apply_force(vec2(0, 100), vec2(0, 0))
-    when 'decelerate'
-      @body.reset_forces()
-      @body.apply_force(vec2(0, -100), vec2(0, 0))
-    when 'em_stop'
-      @body.reset_forces()
-    end
-  end
-
-  def update_physics(dt)
-    @position[0] += 0.5 * @acceleration[0] * dt**2 + @velocity[0] * dt
-    @velocity[0] += @acceleration[0] * dt
-    @position[1] += 0.5 * @acceleration[1] * dt**2 + @velocity[1] * dt
-    @velocity[1] += @acceleration[1] * dt
-
-    @cm.update(dt)
+  def terminal_briefs
+    @terminals.map{|terminal| terminal.brief}
   end
 end
